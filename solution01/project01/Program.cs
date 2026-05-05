0008b37bd3aeba71aace49952f166ac839a916d4
Malakaka <malakalsenine@gmail.com>
2026-05-05 13:55:51 +0400

    Create Movie class with properties and rating validation

--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
+
+        class Movie
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+
+            private int rating;
+
+            public int Rating
+            {
+                get { return rating; }
+                set
+                {
+                    if (value >= 1 && value <= 10)
+                        rating = value;
+                    else
+                        Console.WriteLine("Rating must be between 1 and 10");
+                }
+            }
+
+            public Movie(string title, string genre, int rating)
+            {
+                Title = title;
+                Genre = genre;
+                Rating = rating;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
     }
 }
