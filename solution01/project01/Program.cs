62b212fc9a00d3deffd0bea2fe8099e2f0bdfe9a
Malakaka <malakalsenine@gmail.com>
2026-05-05 14:04:35 +0400

    Create WatchSession class with constructor

--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -74,7 +74,19 @@ namespace project01
                 }
             }
 
+            //WatchSession Class:
 
+            class WatchSession
+            {
+                public string UserName { get; set; }
+                public string MovieTitle { get; set; }
+
+                public WatchSession(string userName, string movieTitle)
+                {
+                    UserName = userName;
+                    MovieTitle = movieTitle;
+                }
+            }
         }
 
 
